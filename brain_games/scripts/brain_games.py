b0cd69670b67f122202501ec0ce97638416bbb4b
Aleksandra Streltsova <alex.hrtnv@gmail.com>
2022-08-08 23:50:25 +0300

    updated brain_games.py (add welcome_user)

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
-def main():
+from cli import welcome_user
+def greet():
     print('Welcome to the Brain Games!')
+
+def main():
+    greet()
+    welcome_user()
+
 if __name__ == '__main__':
-    main()
+    greet()
