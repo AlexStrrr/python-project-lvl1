39e23942c2730658b5ffc6dba4e61f8480950e4b
Aleksandra Streltsova <alex.hrtnv@gmail.com>
2022-08-10 00:03:49 +0300

    added game rules

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
 from cli import welcome_user
+from brain_even import question
 
 
 def greet():
     print('Welcome to the Brain Games!')
 
 
+def game_rules():
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+
+
 def main():
     greet()
     welcome_user()
+    game_rules()
+    question()
 
 
 if __name__ == '__main__':
