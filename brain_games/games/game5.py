01088ffdd64715d5744628183acaa27a9ad95a7e
Aleksandra Streltsova <alex.hrtnv@gmail.com>
2022-08-25 23:05:26 +0300

    updated game5.py

--- brain_games/games/game5.py
+++ brain_games/games/game5.py
@@ -10,7 +10,7 @@ def game5():
     counter = 3
     while index < counter:
         number = randint(1, 100)
-        prime_number = 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, \
+        prime_number = 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,\
             43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
         print(f"Question: {number}")
         index += 1
