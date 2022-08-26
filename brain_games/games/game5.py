407f0b20231e41681310e31d74bb07db70aa0885
Aleksandra Streltsova <alex.hrtnv@gmail.com>
2022-08-27 01:12:53 +0300

    updated game5

--- brain_games/games/game5.py
+++ brain_games/games/game5.py
@@ -13,14 +13,12 @@ def game5():
     counter = 3
     while index < counter:
         number = randint(1, 100)
-        prime_number = 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,\
-            43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+        prime_number = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
         print(f"Question: {number}")
         index += 1
         user_answer = prompt.string('Your answer: ')
-        correct = 'yes' if number == prime_number else 'no'
-        if user_answer == 'yes' and number == prime_number\
-           or user_answer == 'no' and number != prime_number:
+        correct = 'yes' if number in prime_number else 'no'
+        if user_answer == 'yes' and number in prime_number or user_answer == 'no' and number not in prime_number:
             print('Correct!')
         else:
             print(f"'{user_answer}' is wrong answer ;(. \
