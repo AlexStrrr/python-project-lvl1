d60bacb09f0469811734589c3d2ec608667da76c
Aleksandra Streltsova <alex.hrtnv@gmail.com>
2022-09-04 22:23:41 +0300

    added is_prime function

--- brain_games/games/prime_mod.py
+++ brain_games/games/prime_mod.py
@@ -6,8 +6,12 @@ QUESTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def round():
     number = randint(1, 100)
+    question = number
+    correct = 'yes' if is_prime(number) is True else 'no'
+    return question, correct
+
+
+def is_prime(number):
     prime_number = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31,
                     37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
-    question = number
-    correct = 'yes' if number in prime_number else 'no'
-    return question, correct
+    return True if number in prime_number else False
