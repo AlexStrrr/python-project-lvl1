bb38f344f23cf4f683fbb2716357dbb2ee55c0d0
Aleksandra Streltsova <alex.hrtnv@gmail.com>
2022-09-04 22:20:08 +0300

    added is_even function

--- brain_games/games/even_mod.py
+++ brain_games/games/even_mod.py
@@ -7,5 +7,9 @@ QUESTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 def round():
     number = random.randint(1, 100)
     question = number
-    correct = 'yes' if number % 2 == 0 else 'no'
+    correct = 'yes' if is_even(number) is True else 'no'
     return str(question), correct
+
+
+def is_even(number):
+    return True if number % 2 == 0 else False
