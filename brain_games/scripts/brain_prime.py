cc7bb7883944bf3bd3c4706fc1a01a9e73252f94
Aleksandra Streltsova <alex.hrtnv@gmail.com>
2022-09-08 23:45:21 +0300

    renamed modules

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 from brain_games.engine import run
-from brain_games.games import prime_mod
+from brain_games.games import prime
 
 
 def main():
-    run(prime_mod)
+    run(prime)
 
 
 if __name__ == '__main__':
--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 from brain_games.engine import run
-from brain_games.games import progression_mod
+from brain_games.games import progression
 
 
 def main():
-    run(progression_mod)
+    run(progression)
 
 
 if __name__ == '__main__':
